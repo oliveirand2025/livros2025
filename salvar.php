f4d39044131ced559542970f15c6da2b4d12fb4e
oliveirand2025 <oliveirand@gmail.com>
2025-08-12 15:00:32 +0100

    Add files via upload

---
+++ salvar.php
@@ -0,0 +1,27 @@
+<?php
+header('Content-Type: application/json');
+$jsonFile = __DIR__ . '/livros.json';
+
+if(!file_exists($jsonFile)) {
+    file_put_contents($jsonFile, json_encode([]));
+}
+
+$livros = json_decode(file_get_contents($jsonFile), true);
+if (!is_array($livros)) {
+    $livros = [];
+}
+
+$input = json_decode(file_get_contents('php://input'), true);
+if(!$input){
+    echo json_encode(["status"=>"erro","mensagem"=>"Dados inválidos"]);
+    exit;
+}
+
+$livros[] = $input;
+
+if(file_put_contents($jsonFile, json_encode($livros, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE))){
+    echo json_encode(["status"=>"sucesso","mensagem"=>"Livro adicionado"]);
+} else {
+    echo json_encode(["status"=>"erro","mensagem"=>"Falha ao gravar"]);
+}
+?>
